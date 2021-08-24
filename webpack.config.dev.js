5225435df4c563e9b54a509d307ba2aaea504202
Manuel Ricardo <manuelricardohurtdo@gmail.com>
2021-08-24 16:42:06 -0400

    DevTool

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -14,6 +14,7 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     mode: 'development',
+    devtool: 'source-map',
     watch: true,
     resolve: {
         extensions: ['.js'],
